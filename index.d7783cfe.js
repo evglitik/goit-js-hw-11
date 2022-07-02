9398c67986a8273a58e07bdd6d6476586945e932
evglitik <95911893+evglitik@users.noreply.github.com>
2022-07-02 10:04:15 +0000

    Deploying to gh-pages from @ evglitik/goit-js-hw-11@df05ee97baf2188665d9e8f5ecf2bc680c917442 🚀

---
+++ index.d7783cfe.js
@@ -0,0 +1,2 @@
+SmoothScroll({animationTime:600,stepSize:120,accelerationDelta:30,accelerationMax:2,keyboardSupport:!0,arrowScroll:55,pulseAlgorithm:!0,pulseScale:4,pulseNormalize:1,touchpadSupport:!0});
+//# sourceMappingURL=index.d7783cfe.js.map

4a4af4b87a95bc39d688402c5de88a3a5e37de6c
Yevhen Lytvynchuk <95911893+evglitik@users.noreply.github.com>
2022-07-02 14:56:52 +0300

    commit

--- src/js/smouth-scroll.js
+++ src/js/smouth-scroll.js
@@ -1,16 +1,9 @@
 SmoothScroll({
-  // Время скролла 400 = 0.4 секунды
   animationTime: 600,
-  // Размер шага в пикселях
   stepSize: 120,
-  // Дополнительные настройки:
-  // Ускорение
   accelerationDelta: 30,
-  // Максимальное ускорение
   accelerationMax: 2,
-  // Поддержка клавиатуры
   keyboardSupport: true,
-  // Шаг скролла стрелками на клавиатуре в пикселях
   arrowScroll: 55,
 
   pulseAlgorithm: true,

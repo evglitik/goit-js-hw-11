b217f54cf64860fd9791803fedbbbc26a6ed8312
Yevhen Lytvynchuk <95911893+evglitik@users.noreply.github.com>
2022-07-02 15:08:47 +0300

    commits

--- src/js/api-img-card.js
+++ src/js/api-img-card.js
@@ -1,26 +1,36 @@
 import icons from './icon-bootstrap';
 
 function creatCardImages(arrayImg) {
-  const images = arrayImg
-    .map(img => {
-      return `<li><div class="img-container"><img src="${img.webformatURL}" data-href="${img.largeImageURL}" alt="${img.tags}" loading="lazy" /></div>
+    const images = arrayImg
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `<li><div class="img-container"><img src="${webformatURL}" data-href="${largeImageURL}" alt="${tags}" loading="lazy" /></div>
   <div class="info">
     <p class="info-item">
-      <span>${icons.likes}</span><span>${img.likes}</span>
+      <span>${icons.likes}</span><span>${likes}</span>
     </p>
     <p class="info-item">
-      <span>${icons.views}</span><span>${img.views}</span>
+      <span>${icons.views}</span><span>${views}</span>
     </p>
     <p class="info-item">
-      <span>${icons.comments}</span><span>${img.comments}</span>
+      <span>${icons.comments}</span><span>${comments}</span>
     </p>
     <p class="info-item">
-      <span>${icons.downloads}</span><span>${img.downloads}</span>
+      <span>${icons.downloads}</span><span>${downloads}</span>
     </p>
   </div>
 </div></li>`;
-    })
-    .join('');
+        }
+      )
+      .join('');
 
   return images;
 }
